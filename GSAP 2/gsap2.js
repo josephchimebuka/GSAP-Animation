6d8dc2afbfdd0209e82263830db20ee8d7849c98
josephchimebuka <chimebujoseph@gmail.com>
2022-09-07 14:23:10 +0100

    final set of changes

---
+++ GSAP 2/gsap2.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+    $("#fullpage").fullpage({
+        anchors: ["firstpage", "secondpage", "thirdpage"],
+        menu: "#menu",
+        autoScrolling: false
+    });
+});
+
+var controller = new ScrollMagic.Controller();
+
+var scene =  new ScrollMagic.Scene({
+    triggerElement: "#section2"
+})
+
+.setTween("#bg img", {
+    maxWidth: "1100px",
+    top: "30%",
+    left: "80%",
+    opacity: 0.25
+})
+.addTo(controller); 
+
+
+//Tween for section 3
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#section3",
+    duration: "300"
+})
+.setTween("#bg img", {
+    top: "20%",
+})
+
+.addTo(controller);
+
+// $(document).ready(function() {
+// 	$("#fullpage").fullpage({
+// 		anchors: ["firstPage", "secondPage", "thirdPage"],
+// 		menu: "#menu",
+// 		autoScrolling:false
+// 		// responsiveWidth: 750
+// 	});
+// });
+
+
+
+
+// // ScrollMagic: init controller
+// var controller = new ScrollMagic.Controller();
+
+// // Tween - Section 2
+// var scene = new ScrollMagic.Scene({
+// 	triggerElement: "#section2"
+// })
+// // animate color and top border in relation to scroll position
+// .setTween("#bg img", {
+// 	maxWidth: "1200px",
+// 	top: "30%",
+// 	left: "80%",
+// 	opacity: 0.25
+// }) // the tween durtion can be omitted and defaults to 1
+// // .addIndicators({ name: "2 (duration: 100)" }) // add indicators (requires plugin)
+// .addTo(controller);
+
+
+
+
+
+
+// // Tween - Section 3
+// var scene = new ScrollMagic.Scene({
+// 	triggerElement: "#section3",
+// 	duration: 300
+// })
+
+// // animate color and top border in relation to scroll position
+// .setTween("#bg img", {
+// 	top: "20%"
+// }) // the tween duration can be omitted and defaults to 1
+
+// // Add indicators (required plugin)
+// .addTo(controller);
